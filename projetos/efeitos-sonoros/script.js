6eb02bb7840663d2d1049242df782f8ba3d244e1
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-08-05 19:16:38 -0300

    Projeto finalizado - Efeitos Sonoros

---
+++ projetos/efeitos-sonoros/script.js
@@ -0,0 +1,24 @@
+const sounds = ['👏', '🥳','👎','💣', '🐒', '🏝']
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
